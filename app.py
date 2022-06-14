b4e2c0eb73818926ff2b1277f77939d738ca8d81
malik <korpikopan@gmail.com>
2022-06-14 21:45:39 +0200

    update code

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="ML Trainer v1", page_icon=":tada:", layout="wide")
+
+
+# -- HEADER --
+with st.container():
+    st.subheader("ML Trainer v1")
+    st.title("ML Trainer v1")
+    st.write("This app is just a demo to familiarize yourself with basic LinearRegression Model and simple ML regression predictor")
+    st.write("[Learn more >](https://www.twitter.com/__korpikopan__)")
