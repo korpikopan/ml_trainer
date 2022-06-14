0443ffc78e94a08334c2cb869cce8ed5fcb925e3
malik <korpikopan@gmail.com>
2022-06-15 00:25:06 +0200

    update

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge
 
 st.set_page_config(page_title="ML Trainer v1", page_icon=":tada:", layout="wide")
+
+MODEL = {
+    'LinearRegression': LinearRegression, 
+    'LogisticRegression': LogisticRegression,
+    'Ridge': Ridge
+}
+
 
 def create_page(content, page_title=""):
     st.title(page_title)
@@ -22,10 +29,17 @@ with st.sidebar:
             },
     )
 
+    st.write("---")
+    ml_model = st.radio("ML Model", ("LinearRegression", "LogisticRegression", "Ridge"), index=0)
+    if ml_model == 'Ridge':
+        model = MODEL.get(ml_model)(alpha=1.0)
+    else:
+        model = MODEL.get(ml_model)()
+
 with st.container():
     if selected == "ML Trainer":
         content = """This app is just a demo to familiarize yourself with basic 
-        LinearRegression Model and simple ML regression predictor
+        Linear Models (LinearRegression, Ridge, LogisticRegression)
         """
         create_page(content, page_title=selected)
         st.write("---")
@@ -39,29 +53,31 @@ with st.container():
 
             # selection de la target et de la data
             col_num = df.select_dtypes(include='number').columns
-            target_select = st.selectbox("selectionne la colonne a  predire", options=col_num)
+            target_select = st.selectbox("selectionne la colonne à prédire", options=col_num)
             remaining_cols = df.columns.drop(target_select)
             remaining_cols = [col for col in remaining_cols]
-            data_select = st.multiselect("selectionne les colonnes a entrainé", remaining_cols, remaining_cols)
+            data_select = st.multiselect("selectionne les colonnes à entrainer", remaining_cols, remaining_cols)
 
             target = df[target_select]
             data = df[data_select]
 
             st.write("---")
-            sample_train_test_size = st.slider("echantillonage train/test", 0, 100, 80)
+            sample_train_test_size = st.slider("choisissez la taille de l'échantillon d'apprentissage ( recommendation à 70%)", 0, 100, 70)
 
             xtrain, xtest, ytrain, ytest = train_test_split(data, target, train_size=sample_train_test_size/100.0)
             st.write("##")
 
-            train_button = st.button("Entrainer le modele")
+            train_button = st.button("Entrainer le modèle")
 
             if train_button:
-                lr = LinearRegression()
-                lr.fit(xtrain, ytrain)
+                model.fit(xtrain, ytrain)
 
-                score = lr.score(xtest, ytest)
-
-                st.write("Score accuracy = {}".format(score))
+                score = model.score(xtest, ytest)
+                if score >= 0.75:
+                    st.write("Modele de prediction correct! Score de précision à {}".format(score*100.0))
+                
+                else:
+                    st.write("Modele de prediction non performant! Score de précision à {}".format(score*100.0))
 
 
 
